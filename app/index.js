31d0acfadb4d062d5b9c0e6c17cbe6c426b9b4bc
Oxana Theis <otheis@protonmail.com>
2022-11-16 17:17:49 +0100

    test

--- app/index.js
+++ app/index.js
@@ -4,6 +4,17 @@ const cors = require('cors')
 const router = require('./routes')
 
 const app = express()
+
+
+app.use(function (req, res, next){
+    if (req.headers['x-forwarded-proto'] === 'https') {
+      res.redirect('http://' + req.hostname + req.url);
+    } else {
+      next();
+    }
+  });
+
+
 app.use(cors())
 const port = 3000
 
