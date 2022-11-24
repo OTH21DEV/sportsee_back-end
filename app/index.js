e35736026d8adc8508a78d2d18792412b7afc4c5
Oxana Theis <otheis@protonmail.com>
2022-11-24 16:47:45 +0100

    test deploy

--- app/index.js
+++ app/index.js
@@ -6,8 +6,9 @@ const router = require('./routes')
 const app = express()
 app.use(cors())
 const port = 3000
-
+const path = require ('path')
 app.use(router )
+app.use(express.static(path.join(__dirname + "/../public")))
 
 app.listen(port, () => console.log(`Magic happens on port ${port}`))
 
