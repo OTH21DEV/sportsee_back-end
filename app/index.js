3e19bb70747e9368dd31ccf7ae6b4fb68552b5d9
Oxana Theis <otheis@protonmail.com>
2022-11-24 17:50:59 +0100

    test

--- app/index.js
+++ app/index.js
@@ -5,10 +5,11 @@ const router = require('./routes')
 
 const app = express()
 app.use(cors())
-const port = 3000
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000
 const path = require ('path')
 app.use(router )
 app.use(express.static(path.join(__dirname + "/../public")))
 
-app.listen(port, () => console.log(`Magic happens on port ${port}`))
+app.listen(port,host, () => console.log(`Magic happens on port ${port}`))
 
